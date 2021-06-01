2146a93abfdc3f11bb6efdcceb968f3fc47fa6a2
William Sodré <71849007+william0035@users.noreply.github.com>
2021-06-01 11:37:43 -0300

    menu

---
+++ src/components/AddTutorial.js
@@ -0,0 +1,8 @@
+import React from "react";
+const AddTutorial = () => {
+  return (
+    <div >    
+    </div>
+  );
+};
+export default AddTutorial;
---
+++ src/components/TutorialsList.js
@@ -0,0 +1,8 @@
+import React from "react";
+const TutorialsList = () => {
+  return (
+    <div >    
+    </div>
+  );
+};
+export default TutorialsList;
